7d08755d30c27a4252bcd61cae31a3ac2e82c067
herr-code <herrwebj@gmail.com>
2022-04-29 19:39:04 -0500

    GET Crea un endpoint que te regrese un explorer mediando un ID

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ const port = 3000
 
 //HTTP METHODS
 app.get('/v1/explorers', (req, res) => {
-	console.log(`API Explorers GET ALL requests ${ new Date() }`)
-	const explorer1 = { id: 1, name: 'Carlo 1' }
-	const explorer2 = { id: 2, name: 'Carlo 2' }
-	const explorer3 = { id: 3, name: 'Carlo 3' }
-	const explorer4 = { id: 4, name: 'Carlo 4' }
+  console.log(`API Explorers GET ALL requests ${ new Date() }`)
+  const explorer1 = { id: 1, name: 'Carlo 1' }
+  const explorer2 = { id: 2, name: 'Carlo 2' }
+  const explorer3 = { id: 3, name: 'Carlo 3' }
+  const explorer4 = { id: 4, name: 'Carlo 4' }
 
-	const explorers = [explorer1, explorer2, explorer3, explorer4]
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
   res.status(200).json(explorers)
+}); 
+
+//GET Crea un endpoint que te regrese un explorer mediando un ID
+
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`API Explorers GET request ${ new Date() }`)
+  console.log(`Getting explorer with id ${ req.params.id }`)
+  const explorer = { id: 1, name: 'Carlo' }
+
+  res.status(200).json(explorer)
 }); 
 
 // Con esto inicializamos esta app
